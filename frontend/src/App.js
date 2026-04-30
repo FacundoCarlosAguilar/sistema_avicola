26d721608e34087c427199e27d19990333662d10
FacundoAguilar <faguilar@alumnos.unsada.edu.ar>
2026-04-30 00:36:26 -0300

    Actualización
    Nuevos archivos de estructura, API para el sistema avícola.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🐔 Sistema Avícola</h1>
+      <p>Bienvenido al sistema de gestión</p>
+    </div>
+  );
+}
+
+export default App;
